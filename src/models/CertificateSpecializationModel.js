30fcdc48a8deb06bf0b5c1a481927f3a628e5a5a
Noppakun Anantakitthawon <67267679+pro10150@users.noreply.github.com>
2024-03-30 12:53:49 +0700

    Update CertificateSpecializationModel.js

--- src/models/CertificateSpecializationModel.js
+++ src/models/CertificateSpecializationModel.js
@@ -6,5 +6,12 @@ export const CertificateSpecializationModel =  [
         issuedDate: new Date("2023-11-01"),
         issuedOrganization: "Coursera",
         name: "Meta iOS Developer"
+    },  {
+        credentialId: "LL23VAKQ5DD5",
+        credentialUrl: "https://www.coursera.org/account/accomplishments/specialization/certificate/LL23VAKQ5DD5",
+        instructor: "Meta",
+        issuedDate: new Date("2024-03-29"),
+        issuedOrganization: "Coursera",
+        name: "Meta Android Developer"
     }
 ]
