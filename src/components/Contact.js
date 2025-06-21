1d3da99c9be0744defb68099dcfcd28a3836ca80
Noppakun Anantakitthawon <67267679+pro10150@users.noreply.github.com>
2025-06-22 00:42:23 +0700

    dev - Contact - remove telephone number

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,9 +3,9 @@ import ContactSocialMedia from "./List/ContactSocialMedia"
 function Contact() {
     return(
         <div className="contact-body">
-             <big className="contact-header">Interest keeping contact with me?</big>
+             <big className="contact-header">Interest in keeping contact with me?</big>
              <h1 className="contact-subheader">Let keep in touch!</h1>
-             <h2 className="contact-detail">pro10150@gmail.com | +66994757542</h2>
+             <h2 className="contact-detail">pro10150@gmail.com</h2>
              <div className="detail-inline contact-social-media">
                 <ContactSocialMedia  url="https://www.linkedin.com/in/noppakun-anantakitthawon-67b96a183/" image="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/144px-LinkedIn_icon.svg.png?20210220164014" alt="Linkedin logo"/>
                 <div className="contact-social-media-separator"></div>
