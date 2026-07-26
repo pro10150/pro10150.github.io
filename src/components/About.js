cef604e66a8df82533d75229cbbea319cf997292
Noppakun Anantakitthawon <noppakun.antk@gmail.com>
2026-07-26 21:00:32 +0700

    docs: rewrite About copy

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ function About() {
         <div className="section-detail about-body">
             <h1 id="about-me-title">Everything about me!</h1>
             <h3 className="about-me-detail">
-                I'm an iOS Developer based in Bangkok, currently working at ClickNext Ltd. I'm eager to learn new things all the time. Cuz you can always know more, right? I have a dream of working abroad one day in the future, maybe in 2 years or 10. Who knows?
+                I'm an iOS Developer in Bangkok, three years into building apps people actually use — banking, retail, investment. I like the parts of the job where the hard problem isn't the UI. One day I'd like to be doing this abroad; still working out the when.
             </h3>
         </div>
     )
